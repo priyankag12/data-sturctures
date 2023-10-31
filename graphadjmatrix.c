3cc5e1e549ba6f6ab93534da27d3a2092a2743d1
Priyanka Gupta <140226599+priyankag12@users.noreply.github.com>
2023-11-01 00:48:54 +0530

    graphadjmatrix.c

--- graphadjmatrix.c
+++ graphadjmatrix.c
@@ -46,29 +46,3 @@ int main() {
     }
     return 0;
 }
-
-//Print the matrix.
-//void printAdjMatrix(int arr[][v]){
-//	int i,j;
-//	for(i=0;i<v;i++){
-//		printf("%d ",i);
-//		for(j=0;j<v;j++){
-//			printf("%d", arr[i][j]);
-//		}
-//		printf("\n");
-//	}
-//}
-//int main(){
-//	int adjMatrix[v][v];
-//	init(adjMatrix);	
-//	printAdjMatrix(adjMatrix);
-//	printf("\n");
-//	addEdge(adjMatrix,0,1);
-//	addEdge(adjMatrix,0,2);
-//	addEdge(adjMatrix,3,1);
-//	addEdge(adjMatrix,1,1);
-//	addEdge(adjMatrix,1,2);
-//	addEdge(adjMatrix,0,3);
-//	addEdge(adjMatrix,2,1);	
-//	printAdjMatrix(adjMatrix);
-//	return 0;
